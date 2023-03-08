0bee9d769855dcd237da6819d0302fcc7b37e3bf
juliasavritskaya <mnkqn0211>
2023-03-09 01:21:05 +0400

    Make UserService extend Spring UserDetailsService

--- user-service/src/main/java/com/mnkqn/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/mnkqn/userservice/service/impl/UserServiceImpl.java
@@ -7,8 +7,12 @@ import com.mnkqn.userservice.service.UserService;
 import com.mnkqn.userservice.util.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
@@ -18,14 +22,35 @@ public class UserServiceImpl implements UserService {
 
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
 
     @Override
     public UserDto createUser(UserDto user) {
 
         user.setUserId(UUID.randomUUID().toString());
-        user.setEncryptedPassword("enc pas");
+        user.setEncryptedPassword(encoder.encode(user.getPassword()));
+
         User createdUser = userRepository.save(userMapper.fromUserDtoToUserEntity(user));
 
         return userMapper.fromUserEntityToUserDto(createdUser);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username);
+        if (user == null) throw new UsernameNotFoundException(username);
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getEncryptedPassword(), true, true, true, true, new ArrayList<>());
+
+    }
+
+    @Override
+    public UserDto getUserDetailsByEmail(String email) {
+
+        User user = userRepository.findByEmail(email);
+        if (user == null) throw new UsernameNotFoundException(email);
+
+        return userMapper.fromUserEntityToUserDto(user);
     }
 }
