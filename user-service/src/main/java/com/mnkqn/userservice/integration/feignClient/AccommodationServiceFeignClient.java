59c0541f47bcbb7456f8d45d91e88781d6bda8df
juliasavritskaya <mnkqn0211>
2023-03-10 01:28:02 +0400

    Configure Feign Client for user-service

---
+++ user-service/src/main/java/com/mnkqn/userservice/integration/feignClient/AccommodationServiceFeignClient.java
@@ -0,0 +1,11 @@
+package com.mnkqn.userservice.integration.feignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "accommodations-ws")
+public interface AccommodationServiceFeignClient {
+
+    @GetMapping
+    void getAccommodations();
+}
