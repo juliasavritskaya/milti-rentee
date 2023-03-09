0e327bfc67124af907b5e44bfbcaae68cf17b160
juliasavritskaya <mnkqn0211>
2023-03-10 01:27:22 +0400

    Add accommodation entity and db script

---
+++ accommodation-service/src/main/resources/db/migration/V1__Create_table_accommodations.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "accommodations"
+(
+    "id"          bigserial     NOT NULL PRIMARY KEY,
+    "name"        varchar(255)  NOT NULL,
+    "description" varchar(1000) NOT NULL,
+    "price"       int           NOT NULL,
+    "rooms"       int2          NOT NULL,
+    "bedrooms"    int2          NOT NULL,
+    "user_id"     varchar       NOT NULL
+);
