635cd484323ed18a31466514f2066db9a680f8e1
Julia Savritskaya <55387479+juliasavritskaya@users.noreply.github.com>
2023-03-10 05:36:40 +0400

    Delete rentee directory

--- rentee/src/main/java/com/mnkqn/rentee/domain/entity/AccommodationStatus.java
+++
@@ -1,8 +0,0 @@
-package com.mnkqn.rentee.domain.entity;
-
-public enum AccommodationStatus {
-    AVAILAVLEFORRENT,
-    INACTIVE,
-    RESERVED,
-    RENTED
-}
--- rentee/src/main/java/com/mnkqn/rentee/service/AddressService.java
+++
@@ -1,8 +0,0 @@
-package com.mnkqn.rentee.service;
-
-import com.mnkqn.rentee.domain.dto.AddressDto;
-
-public interface AddressService {
-
-    AddressDto getById(Long id);
-}
