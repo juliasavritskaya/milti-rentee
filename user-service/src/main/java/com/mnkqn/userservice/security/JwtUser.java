91e57352dcc1ff7eed317cabdced18496e38ef8a
juliasavritskaya <mnkqn0211>
2023-03-10 06:30:34 +0400

    Add JwtUser

---
+++ user-service/src/main/java/com/mnkqn/userservice/security/JwtUser.java
@@ -0,0 +1,57 @@
+package com.mnkqn.userservice.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class JwtUser implements UserDetails {
+
+    private final String uuid;
+    private final String username;
+    @JsonIgnore
+    private final String password;
+    @JsonIgnore
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
